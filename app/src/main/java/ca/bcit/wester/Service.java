b59db5eb6086b41ffc384f92aed5c570307e4566
Chonjou Chiang <chonjou@gmail.com>
2017-10-17 09:44:10 -0700

    Added setter and getter for the service

--- app/src/main/java/ca/bcit/wester/Service.java
+++ app/src/main/java/ca/bcit/wester/Service.java
@@ -9,9 +9,25 @@ public class Service {
     private String name;
     private String category;
 
-    public Service(double longi, double lat, double alt, String name, String category) {
-        this.location = new Location(longi, lat, alt);
+    public Service(double longit, double lat, double alt, String name, String category) {
+        this.location = new Location(longit, lat, alt);
         this.name = name;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
         this.category = category;
     }
 }
